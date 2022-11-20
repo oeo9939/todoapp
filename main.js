af60c80b912560af8a22a05ba30df8ca33cc88b0
Teo <mgttmich@gmail.com>
2022-11-20 19:51:44 +0200

    Add form.eventListener, createEl li, innerText

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const todos = document.getElementById("todos");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const todo = input.value;
+
+    if(todo) {
+        const todoEl = document.createElement("li");
+        todoEl.innerText = todo;
+        todos.appendChild(todoEl);
+    }
 });
