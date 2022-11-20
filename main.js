da362374f6524df07bbbc7632d3ea07dde6830bd
Teo <mgttmich@gmail.com>
2022-11-20 20:31:06 +0200

    Add remove right click listener, set css for placeholder

--- main.js
+++ main.js
@@ -5,11 +5,24 @@ const todos = document.getElementById("todos");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const todo = input.value;
+    const todoText = input.value;
 
-    if(todo) {
+    if(todoText) {
         const todoEl = document.createElement("li");
-        todoEl.innerText = todo;
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+        });
+
         todos.appendChild(todoEl);
+
+        input.value = "";
     }
 });
