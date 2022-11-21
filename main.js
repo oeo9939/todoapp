c7d60a2de72b396591d116cc64500a535c2cb6a6
Teo <mgttmich@gmail.com>
2022-11-21 20:02:13 +0200

    try update local storage

--- main.js
+++ main.js
@@ -6,18 +6,22 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     addTodo();
+    
 });
 
 function addTodo() {
     
     const todoText = input.value;
+    updateLS(todoText.value);
 
     if(todoText) {
         const todoEl = document.createElement("li");
         todoEl.innerText = todoText;
+        
 
         todoEl.addEventListener("click", () => {
             todoEl.classList.toggle("completed");
+            
         });
 
         todoEl.addEventListener("contextmenu", (e) => {
@@ -29,5 +33,19 @@ function addTodo() {
         todos.appendChild(todoEl);
 
         input.value = "";
+
+        
     }
+}
+
+function updateLS() {
+    // const todoKey = ;
+    const notes = document.querySelectorAll("li");
+    // let notesTarget = notes.textContent;
+    // console.log(notes);
+    localStorage.setItem("aoua", JSON.stringify(notes));
+
+
+
+    
 }
